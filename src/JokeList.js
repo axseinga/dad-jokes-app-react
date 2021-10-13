c174a87d6f5e7875fc96472a0355ef99b730ae3d
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-10-13 11:07:25 +0100

    add api

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const JokeList = () => {
+    useEffect(() => {
+        async function getData() {
+            let res = await axios.get("https://icanhazdadjoke.com/", {
+                headers: { Accept: "application/json" },
+            });
+            console.log(res.data.joke);
+        }
+        getData();
+    }, []);
+
     return (
         <div>
             <h1>Dad Jokes</h1>
