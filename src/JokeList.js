7d2b5f7e42f61f502be630c39dcdbbfd3bc64cd9
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-10-13 11:52:14 +0100

    refactor api function to work with useEffect and return array of strings

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,15 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const JokeList = () => {
+    const [dadJokes, setDadJokes] = useState([]);
+    const numJokesToGet = 10;
+
     useEffect(() => {
-        async function getData() {
-            let res = await axios.get("https://icanhazdadjoke.com/", {
+        function getData() {
+            const promise = fetch("https://icanhazdadjoke.com/", {
                 headers: { Accept: "application/json" },
             });
-            console.log(res.data.joke);
+            return promise
+                .then((resp) => {
+                    if (resp.ok) {
+                        return resp.json();
+                    }
+                    return Promise.reject(resp);
+                })
+                .then((data) => {
+                    const joke = data.joke;
+                    return joke;
+                })
+                .catch((err) => console.log(err));
         }
-        getData();
+
+        async function getJokes() {
+            let jokes = [];
+            while (jokes.length < numJokesToGet) {
+                const res = await getData();
+                jokes.push(res);
+            }
+            setDadJokes(jokes);
+        }
+        getJokes();
     }, []);
 
     return (
